cebccca68e4962787cd0c7967896b4ddfa0685a2
M Tharun <tharungowda502@gmail.com>
2026-02-07 10:41:47 +0530

    Add files via upload

---
+++ primitive.java
@@ -0,0 +1,18 @@
+public class primitive
+{
+    public static void main(String[] args)
+    {
+        byte b=120;
+        short a=150;
+        int c=10000;
+        long l=123456;
+        float f=12.12345f;
+        double d=150.9876d;
+        System.out.println(b);
+        System.out.println(a);
+        System.out.println(c);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+    }
+}
