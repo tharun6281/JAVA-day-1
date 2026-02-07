d038416164cbbf8011e8088ea8f7728f2b817a7f
M Tharun <tharungowda502@gmail.com>
2026-02-07 14:52:38 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,37 @@
+
+
+public class sum
+{
+    public static void main(String[] args)
+    {
+        int a = 10;
+        int b = 30;
+        System.out.println(a+b);
+        int a1=-8;
+        if(a1>8)
+        {
+           System.out.println("positive"); 
+        }
+        else
+        {
+            System.out.println("negative");
+        }
+        int a2=6;
+        if(a2>0)
+        {
+            System.out.println("even");
+        }
+        else
+        {
+            System.out.println("odd");
+        }
+        if(a2%3==0)
+        {
+            System.out.println("is divisable by 3.");
+        }
+        else
+        {
+            System.out.println("is not divisable by 3.");
+        }
+    }
+}
