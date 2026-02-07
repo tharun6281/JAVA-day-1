e5b430bb14529babdf5fcd476b60a216283b82c9
M Tharun <tharungowda502@gmail.com>
2026-02-07 10:12:24 +0530

    Add files via upload

---
+++ literal.java
@@ -0,0 +1,14 @@
+public class literal
+{
+    public static void main(String []args)
+    {
+        int a=100;
+        int b=051;
+        int c=0x2A;
+        int d=0b100;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
