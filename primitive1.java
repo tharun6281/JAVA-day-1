cfe1eba1739c2458f1f3218ebebb0238ce0f6e59
M Tharun <tharungowda502@gmail.com>
2026-02-07 11:03:19 +0530

    Add files via upload

---
+++ primitive1.java
@@ -0,0 +1,16 @@
+public class primitive1
+{
+    public static void main(String[] args) 
+    {
+      String str="tharun";
+      System.out.println("Student name:"+str);
+      int age=19;
+      System.out.println("Student age:"+age);
+      char grade='A';
+      System.out.println("Student grade:"+grade);
+      int marks=800;
+      System.out.println("Student marks:"+marks);
+      String str1="pass";
+      System.out.println("Student pass:"+str1);
+    }
+}
