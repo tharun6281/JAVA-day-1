235b50cdab1f9cd4a2ce3f9ef6e5be3c7b2f88a6
M Tharun <tharungowda502@gmail.com>
2026-02-07 15:35:42 +0530

    Add files via upload

---
+++ swap.java
@@ -0,0 +1,15 @@
+public class Swap
+{
+    public static void main(String[] args)
+    {
+        int a = 10;
+        int b = 30;
+        int temp;
+        temp=a;
+        a=b;
+        b=temp;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
+
